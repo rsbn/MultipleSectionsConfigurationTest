3fabca1e21689e68e7004cfad6cca91a867e39f0
rsbn <paulo@rsbn.com.br>
2019-03-15 16:04:11 -0300

    Initial

--- testMultipleSectionsConfigSection/Program.cs
+++ testMultipleSectionsConfigSection/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,19 @@ namespace testMultipleSectionsConfigSection
     {
         static void Main(string[] args)
         {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            ConfigurationSectionGroup myCustomConfig = config.SectionGroups["MyCustomConfig"];
+            foreach (ConfigurationSection section in myCustomConfig.Sections)
+            {
+                NameValueCollection sectionSettings = ConfigurationManager.GetSection(section.SectionInformation.SectionName) as NameValueCollection;
+                foreach (string item in sectionSettings)
+                {
+                    var value = sectionSettings[item];
+                    Console.WriteLine($"{ section.SectionInformation.Name } => { item } : { value }");
+                }
+            }
+            Console.WriteLine("Press any key to conetinue...");
+            Console.ReadKey();
         }
     }
 }
